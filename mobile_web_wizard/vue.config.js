e7db6b5abc77d72b586c7bcb434309f664dcf902
JsonTyler <jasontylerrodriguez@gmail.com>
2024-01-21 19:35:44 -0800

    fix output directory

--- mobile_web_wizard/vue.config.js
+++ mobile_web_wizard/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: 'mobile_web_wizard/public/dist',
+  outputDir: 'public/dist',
 })
